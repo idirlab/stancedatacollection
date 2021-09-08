c6e5faf55ca6d2f44fc8db20300c65764895b02d
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-09-08 02:48:49 -0500

    fix database

--- check_sign_in_information.php
+++ check_sign_in_information.php
@@ -4,20 +4,20 @@
 	$password = $_REQUEST["password"];
 
 	$encrypted_password = '"'.md5($password).'"';
-	// var_dump($encrypted_password);
+	var_dump($encrypted_password);
 	include_once("db.php");
 	$db_tmp = getConnect();
-	// var_dump($db_tmp);
+	var_dump($db_tmp);
 	$sql = gen_select_query(array('username', 'count(*) as count'), array('User'), array('username = '.$username, 'password = '.$encrypted_password));
 	$sql = "select * from User;";
-	// var_dump($sql);
+	var_dump($sql);
 	$results = execute($sql, array(), PDO::FETCH_ASSOC);
-	// var_dump($results);
+
 	if(strcmp($results[0]['count'], "0") == 0) echo "-1";#wrong information
 
 	$sql = gen_select_query(array('verification'), array('User'), array('username = '.$username, 'password = '.$encrypted_password));
 	$results = execute($sql, array(), PDO::FETCH_ASSOC);
-	// var_dump($results);
+
 	if(strcmp($results[0]['verification'], "verified") == 0)
 	{	
 		$_SESSION['username'] = $username;
