fc7c48060ecc29d48e9a570a45b0e32702716808
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-09-08 02:51:09 -0500

    fix database

--- check_sign_in_information.php
+++ check_sign_in_information.php
@@ -5,7 +5,9 @@
 
 	$encrypted_password = '"'.md5($password).'"';
 	var_dump($encrypted_password);
-	include_once("./db.php");
+	// include_once("./db.php");
+	include_once dirname(__FILE__) . '/../db.php';
+	
 	$db_tmp = getConnect();
 	var_dump($db_tmp);
 	$sql = gen_select_query(array('username', 'count(*) as count'), array('User'), array('username = '.$username, 'password = '.$encrypted_password));
